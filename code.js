86884bddd610ec3a1410426e229133c93e0fa0a8
xmahbub <droidshakib@gmail.com>
2023-10-07 00:18:34 +0600

    Animation

--- code.js
+++ code.js
@@ -8,21 +8,15 @@ var tl = gsap.timeline()
 var tl2 = gsap.timeline()
   
 
-tl.fromTo(".container img", {opacity: 0,  }, {opacity: 100, duration: 2,stagger:{
-    each:0.2,
-    from:"start",
-    
-
-}},)
+tl.to('.character',4,{backgroundPosition: "-8700px",ease:SteppedEase.config(29)}).to('.emty',1,{},)
 .fromTo(".ballsize",{opacity:0},{opacity:100,duration:3,motionPath:{
     path:"M1 39.8142C90.7913 42.3226 137.496 48.879 216.258 67C326.313 35.5817 388.858 18.7393 516 3",
-    align: "#ballpath",},},
-    "-=2.5"
+    align: "#ballpath",},},3
 )
 
 gsap.fromTo(".matchmain",{y:-40,opacity:0},{y:80,opacity:100,duration:1,ease:"back"},6)
 
-tl.repeat(-1)
+tl.repeat(4)
 
 document.getElementById("button").onclick = ()=>{
     console.log("clicked")
